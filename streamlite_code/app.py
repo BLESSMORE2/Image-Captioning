9b726435fd4862ce839f3f879da4ffba93be4d51
BLESSMORE2 <76495184+BLESSMORE2@users.noreply.github.com>
2023-09-30 10:26:41 +0200

    Update app.py

--- streamlite_code/app.py
+++ streamlite_code/app.py
@@ -92,7 +92,8 @@ def main():
         
         # Convert the video_file object to a file path
         #video_path = os.path.join(output_frame_dir, "uploaded_video.mp4")
-        video_path = os.path.join(str(output_frame_dir), "uploaded_video.mp4")
+        #video_path = os.path.join(str(output_frame_dir), "uploaded_video.mp4")
+        video_path = output_frame_dir/"uploaded_video.mp4"
         
 
         with open(video_file, "wb") as f:
