6d972e50b0b968c74438fc8658aa4a17cf06bf3e
BLESSMORE2 <76495184+BLESSMORE2@users.noreply.github.com>
2023-10-01 19:37:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -45,6 +45,17 @@ openai.api_key = "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"
 
 
 
+# Get the current date
+current_date = datetime.datetime.now().date()
+
+# Define the date after which the model should be set to "gpt-3.5-turbo"
+target_date = datetime.date(2024, 6, 12)
+# Set the model variable based on the current date
+if current_date > target_date:
+    llm_model = "gpt-3.5-turbo"
+else:
+    llm_model = "gpt-3.5-turbo-0301"
+        
 def get_completion(prompt, model=llm_model):
     messages = [{"role": "system", "content": "You are a helpful assistant that generates video descriptions."},
                 {"role": "user", "content": prompt}]
